8e8bb8e01e153e4cb4217f13211cd5a5b327f38f
Jaime Martinez <jjmtz10@yahoo.com>
2026-04-19 12:58:35 -0500

    Basic program shell and command line validation

--- cache_sim.cpp
+++ cache_sim.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
 
+int main(int argc, char* argv[]) {
+
+      if (argc !=4)
+        {
+          std::cout << "usage: ./cache_sim num_entries associativity memory_reference_file" << std::endl;
+          return 1;
+        }
+
+        return 0;
+}
