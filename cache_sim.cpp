438c3e6ff277920999309f575138f47bf4bfd2c8
Jaime Martinez <jjmtz10@yahoo.com>
2026-04-19 13:02:05 -0500

    Parse cache configuration parameters from command line

--- cache_sim.cpp
+++ cache_sim.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[]) {
 
-      if (argc !=4)
+    if (argc !=4)
         {
-          std::cout << "usage: ./cache_sim num_entries associativity memory_reference_file" << std::endl;
-          return 1;
+            std::cout << "usage: ./cache_sim num_entries associativity memory_reference_file" << std::endl;
+            return 1;
         }
+
+    int num_entries = std::stoi(argv[1]);
+    int associativity = std::stoi(argv[2]);
+    std::string memory_reference_file = argv[3];
+
+    std::cout << "Number of entries: " << num_entries << std::endl;
+    std::cout << "Associativity: " << associativity << std::endl;
+    std::cout << "Memory reference file: " << memory_reference_file << std::endl;
+
 
         return 0;
 }
